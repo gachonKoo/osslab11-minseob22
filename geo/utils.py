3bc1e1cd678c0e6f01ffd022b026b4d906701ab8
mshan-brm99 <140988048+minseob22@users.noreply.github.com>
2024-11-17 03:11:50 +0900

    Update utils.py

--- geo/utils.py
+++ geo/utils.py
@@ -1,11 +1,11 @@
-import geo.utils as utils
+import math
 
-# calculate the length of hypotenuse (c) when a=3 and b=4
-a, b = 3, 4
-c = utils.pythagoras(a, b)
-print('c =', c)
 
-# calculate the area of a circle with radius r=10
-r = 10
-area = utils.circle(r)
-print('area =', area)
+def pythagoras(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+
+def circle(r):
+    area = math.pi * r**2
+    return area
